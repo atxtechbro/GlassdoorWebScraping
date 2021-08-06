ff0ec227d55623d964faee0c920f3a466ffffc6a
mojo-py <84004324+mojo-py@users.noreply.github.com>
2021-08-06 15:05:40 -0500

    Add files via upload

---
+++ ScrapingGlassdoor.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+from bs4 import BeautifulSoup
+import json
+import re
+import requests
+from random import randint
+from time import sleep
+
+locID = 0
+def turnThePage(locID):
+    global bizBook, j
+    headers={'User-Agent': 'Mozilla/5.0'}
+    bizBook = {}
+    while True:
+        url = "https://www.glassdoor.com/Explore/browse-companies.htm?overall_rating_low=0&page=1&isHiringSurge=0&locId={}&locType=N&locName=US".format(locID)
+        r = requests.get(url, headers=headers)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        a = str(soup.find('article').find('script').next.replace('\n', '').strip())
+        regex = r"JSON MESSAGE BUNDLE - do not remove\"}},(.*)"
+        regex_response = re.search(regex, a).group(1)
+                
+        d = json.loads('{' + regex_response[:-1])['apolloState']
+        json_data = json.dumps(d)
+        j = json.loads((json_data.replace(r"\\", '')))
+                
+        try:
+            for k in j['ROOT_QUERY'].keys():
+                if k.startswith('employerJobsInfo'):
+                    start = k.find('id')
+                    end = k.find('name')
+                    id_ = k[start+4:end-2]
+                    biz = k[end+7:-4]
+                    print(biz)
+                    bizBook[id_] = biz
+        except KeyError as e:
+            pass
+        locID += 10
+        print(len(bizBook))
+        print(bizBook)
+        sleep(randint(8,13))
+turnThePage(locID)
+
+
+# In[ ]:
+
+
+
+
