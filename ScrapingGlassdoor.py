92b87c94503efd9386e25fb8317ecf4cebb02307
Morgan Joyce <84004324+mojo-py@users.noreply.github.com>
2021-08-12 17:24:39 -0500

    Update ScrapingGlassdoor.py

--- ScrapingGlassdoor.py
+++ ScrapingGlassdoor.py
@@ -2,44 +2,76 @@ import json
 import random
 import requests
 import time
+
 bizBook = {}
-pageNumber = 1
-#create bizBook file and initiate pageNumber to 1
-with open('BizBook.json', 'w') as f:
+pageNumber = 95
+sectorBase = 10000
+sectorIndex = sectorBase + 1
+
+print('page number initialized to ', pageNumber)
+
+with open('bizBook.json', 'w') as f:
     f.write(json.dumps(bizBook))
+with open('bizBook.json', 'r') as f:
+    businesses = f.read()
+    bizBook = json.loads(businesses) 
+with open('industries.json', 'r') as f:
+    sectors_ = f.read()
+    sectors = json.loads(sectors_)
+    
+    
+headers = {
+    'Accept-Encoding': 'gzip, deflate, sdch',
+    'Accept-Language': 'en-US,en;q=0.8',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Referer': 'http://www.wikipedia.org/',
+    'Connection': 'keep-alive',
+    }  
+    
+sectorList = []
+for key in sectors.keys():
+    sectorList.append(key)
 
-while True:
-    
-    with open('BizBook.json', 'r') as f:
-        businesses = f.read()
-        bizBook = json.loads(businesses)
+while True:   
+    startUrl = f'https://www.glassdoor.com/seo/ajax/ugcSearch.htm?minRating=0&maxRating=5&numPerPage=100&'
 
-    url = f'https://www.glassdoor.com/seo/ajax/ugcSearch.htm?minRating=0&maxRating=5&numPerPage=100&pageRequested={pageNumber}&domain=glassdoor.com&surgeHiring=false'
-    pageNumber += 1
-
-    
-    #avoid getting red flagged
-    r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'}) #vs {'User-Agent': 'python-requests/2.26.0'}
-    assert r.status_code == 200
-    
-    
+    if pageNumber >= 100:
+        pageNumber = 1
+        sectorIndex += 1
+    else:
+        pageNumber += 1
+        pass
+    urlAppendage = f'pageRequested={pageNumber}&domain=glassdoor.com&surgeHiring=false&sectorIds={sectorIndex}'
+    url = startUrl + urlAppendage
+     
+      
+    r = requests.get(url, headers=headers) #avoid getting red flagged
     jsonResponse = json.loads(r.text)
+    
+    if pageNumber > jsonResponse['employerSearchResponse']['numPagesAvailable']:
+        sectorIndex += 1
+        pageNumber = 1
+        urlAppendage = f'pageRequested={pageNumber}&domain=glassdoor.com&surgeHiring=false&sectorIds={sectorIndex}'
+        url = startUrl + urlAppendage
+        r = requests.get(url, headers=headers) #avoid getting red flagged
+        jsonResponse = json.loads(r.text)
+    else:
+        pass
+    
     records = jsonResponse['employerSearchResponse']['results']
     #now all our data in contained in records which are 100 mini dictionaries
     
     #extracting the exact corporateID, corporateName from each of the 100 records and saving them to a scannedRecords dictionary which will end up containing all of them
-    scannedRecords = {}
+    beforeBizBookLength = len(bizBook)
     for rec in records:
         corporateID, corporateName = rec['id'], rec['name']
-        scannedRecords[corporateID] = corporateName
-        bizBook.update(scannedRecords) #add new data to our bizBook and save it to local storage
-    with open('BizBook.json', 'w') as f:
-        f.write(json.dumps(bizBook, indent = 2))
+        bizBook[corporateID] = corporateName
+    afterBizBookLength = len(bizBook)
+    collectedPairsThisPage = afterBizBookLength - beforeBizBookLength
+
+    print('p.', pageNumber, sectors[f'{sectorIndex}'], f'(+{collectedPairsThisPage})')
     
-    #scraping progress statistics
-    mileMarker = len(bizBook)
-    destination = jsonResponse['employerSearchResponse']['numRecordsAvailable']
-    percentageComplete = mileMarker / destination
-    pctProg = '{:.4%}'.format(percentageComplete) #percentage of scraped companies
-    print(pctProg, str(mileMarker) + ' / ' + str(destination))
-    time.sleep(random.randint(5,10))
+    with open('bizBook.json', 'w') as f:
+        f.write(json.dumps(bizBook, indent = 2))
+    time.sleep(random.randint(4,5))
