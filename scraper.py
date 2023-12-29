b44075c36344feae01b5ebabd833aed38a7080d0
Morgan Joyce <morganj2k@gmail.com>
2023-12-29 16:21:33 -0600

    optimize graphql query request headers for improved 200 response using backend-centric approach

--- scraper.py
+++ scraper.py
@@ -1,54 +1,46 @@
 import json
 
 import requests
-from bs4 import BeautifulSoup
 
-# Load existing industries or start with an empty dictionary
-try:
-    with open('industries.json', 'r') as f:
-        industries = json.load(f)
-except json.JSONDecodeError:
-    industries = {}
+url = 'https://www.glassdoor.com/graph'
+headers = {
+    'authority': 'www.glassdoor.com',
+    'method': 'POST',
+    'path': '/graph',
+    'scheme': 'https',
+    'accept': '*/*',
+    'accept-encoding': 'gzip, deflate, br',
+    'accept-language': 'en-US,en;q=0.9',
+    'apollographql-client-name': 'company.explorer',
+    'apollographql-client-version': '3.18.12',
+    'content-length': '1656',
+    'content-type': 'application/json',
+    'cookie': 'asst=1703884286.0; gdId=d9f49a39-3a83-4254-9b05-ee508425e1fb; GSESSIONID=undefined; JSESSIONID=8BD9728688F35A560BE14C52BF9086B9; cass=1; companiesClicked=true; AWSALB=a2RFma+/tBhq9j3ija3daoNCuK1UH8HKQd7nivlpngGq5V31uV9XXlHARG405MIDvWKrW8xoY9GyzGxzJPJfU3+lpqqvOvIKKZ6GiAmJcc3TnSW1DDP0q9+7n8Fc; AWSALBCORS=a2RFma+/tBhq9j3ija3daoNCuK1UH8HKQd7nivlpngGq5V31uV9XXlHARG405MIDvWKrW8xoY9GyzGxzJPJfU3+lpqqvOvIKKZ6GiAmJcc3TnSW1DDP0q9+7n8Fc; gdsid=1703884287447:1703887414513:69AF0D0737F607A97F0296018DAD700E; __cf_bm=7ojqBHf.3Na.L6AoKxk6Rk4PkfZ5tXzSp7Vd0C_nrs0-1703887414-1-AR44LMU/L9o7qFj5QfVhf1Nr3q/83wdIFwM0amtWv+1P/C9zHqJLZBoDYp9Vjwp4cUl1pnUX2WIot7YrLAaz9nQTkXd1pPp6Zl2XCFoq9AVr; _cfuvid=ArL2v6Qdq7.cGjhG5waoyDU8c1BitOizDPildIZEHGA-1703887414645-0-604800000',
+    'gd-csrf-token': 'W4qI4GJFkb3rDWO0KbLjUw:EF2AITEIXWTdlvXJRskcgzt6C-uTRWH4WurHQRk4o4bpFUMmBcxsLiAoe_qNjrulIqPKZF7-DI9rBQ644Vn2cA:JVJdlO9md9osN-m0WbtgFcqEyi1ScjPDjD2siKH1-_k',
+    'origin': 'https://www.glassdoor.com',
+    'referer': 'https://www.glassdoor.com/',
+    'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-origin',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+}
 
-# Load existing companies or start with an empty dictionary
-try:
-    with open('companies.json', 'r') as f:
-        companies = json.load(f)
-except json.JSONDecodeError:
-    companies = {}
+# Your GraphQL query
+graphql_query = {
+  "operationName": "FeaturedJobsQuery",
+  "variables": {
+    "keyword": "",
+    "locationType": "COUNTRY",
+    "locationId": 1,
+    "adSlotName": "search-reviews-lf-right",
+    "numPerPage": 5
+  },
+  "query": "query FeaturedJobsQuery($keyword: String!, $locationType: LocationTypeEnum, $locationId: Int!, $adSlotName: String!, $numPerPage: Int!) { jobListings(contextHolder: {adSlotName: $adSlotName, searchParams: {numPerPage: $numPerPage, searchType: FJ, skipFeaturedJobs: true, skipUrgencyLabel: true, locationType: $locationType, locationId: $locationId, keyword: $keyword}}) { jobListings { jobview { job { listingId __typename } overview { id name: shortName squareLogoUrl(size: SMALL) __typename } rating { starRating __typename } header { ageInDays jobLink jobTitleText locationName payCurrency payPeriod payPercentile10 payPercentile90 salarySource __typename } __typename } __typename } __typename } }"
+}
 
-# Start URL
-start_url = 'https://www.glassdoor.com/Reviews/index.htm?overall_rating_low=3.5&page='
+response = requests.post(url, headers=headers, data=json.dumps(graphql_query))
 
-# Iterate over pages
-for page in range(1, 101):  # Adjust the range as needed
-    response = requests.get(start_url + str(page))
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    # Replace with your own logic to extract industries and companies
-    new_industries = soup.find_all('div', class_='industry')
-    new_companies = soup.find_all('div', class_='company')
-
-    for industry in new_industries:
-        industry_id = industry.get('id')
-        industry_name = industry.get_text()
-
-        # Add new industry to the dictionary if it's not already there
-        if industry_id not in industries:
-            industries[industry_id] = industry_name
-
-    for company in new_companies:
-        company_id = company.get('id')
-        company_name = company.get_text()
-
-        # Add new company to the dictionary if it's not already there
-        if company_id not in companies:
-            companies[company_id] = company_name
-
-    # Save the updated industries back to the file
-    with open('industries.json', 'w') as f:
-        json.dump(industries, f, indent=2)
-
-    # Save the updated companies back to the file
-    with open('companies.json', 'w') as f:
-        json.dump(companies, f, indent=2)
+print(response.text)
